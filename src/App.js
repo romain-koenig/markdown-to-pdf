a424fe589b31b48316dcb99a9fc0b43f53747e91
Romain KOENIG <romain@kromatic.fr>
2023-05-16 11:16:52 +0200

    WIP on instructions

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Markdown from 'markdown-to-jsx';
-
-import pdfMake from "pdfmake/build/pdfmake";
-import pdfFonts from "pdfmake/build/vfs_fonts";
-pdfMake.vfs = pdfFonts.pdfMake.vfs;
-
 
 function App() {
   const [markdown, setMarkdown] = useState('');
+
+  useEffect(() => {
+    retrieveInitialMarkdown();
+  }, []);
 
   const handleInputChange = (event) => {
     setMarkdown(event.target.value);
@@ -21,6 +20,10 @@ function App() {
     },
   };
 
+  const handleReset = () => {
+    retrieveInitialMarkdown();
+  };
+
   const handlePrint = () => {
     const printWindow = window.open('', '_blank');
     const markdownOutput = document.getElementById('markdownOutput').innerHTML;
@@ -31,11 +34,25 @@ function App() {
           <title>Print</title>
           <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
           <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+          <style>
+          @media print {
+            .printInstructions {
+              display: none;
+            }
+          }
+          </style>
         </head>
         <body>
         <div class="container">
-        ${markdownOutput}
+          ${markdownOutput}
+          <div class="printInstructions">
+            <p><em>Please use your browser's print functionality to print this page. This is typically accessible by pressing Ctrl+P (Windows) or Command+P (Mac). You can also click on the button below.</em></p>
+            <button class="btn btn-primary printInstructions" onclick="window.print()">Print</button>
+            <button class="btn btn-secondary printInstructions" onclick="window.close()">Back to editor</button>
+
+          </div>
         </div>
+
         <script type="text/javascript">
         window.onload = function() {
           window.print();
@@ -65,6 +82,7 @@ function App() {
               value={markdown}
               onChange={handleInputChange}
             />
+            <button className="btn btn-primary" onClick={handleReset}>Reset demo text</button>
           </div>
           <div className="col-6">
             <div id="markdownOutput">
@@ -82,6 +100,12 @@ function App() {
 
     </div>
   );
+
+  function retrieveInitialMarkdown() {
+    fetch('/instructions.md')
+      .then(response => response.text())
+      .then(data => setMarkdown(data));
+  }
 }
 
 export default App;
