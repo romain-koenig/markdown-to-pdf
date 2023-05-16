a2b52cfc277c5b6a60a77d09725d6505ae76e206
Romain KOENIG <romain@kromatic.fr>
2023-05-16 14:33:08 +0200

    Removed external MD
    Unfortunately this doesn't work on GitHub Pages

--- src/App.js
+++ src/App.js
@@ -2,6 +2,74 @@ import React, { useState, useEffect } from 'react';
 import Markdown from 'markdown-to-jsx';
 import './App.css';
 
+const instructions = `
+# Markdown Editor
+
+This is a simple app to edit and print (as PDF?) markdown files.
+
+## Markdown Cheatsheet
+
+### Headers
+
+# H1
+## H2
+### H3
+#### H4
+
+### Emphasis
+
+*italic*  
+**bold**  
+***bold italic***
+
+### Footnotes
+
+Footnotes[^1] have a label[^2] and the footnote's content.
+
+[^1]: This is a footnote content.
+
+[^2]: This is another footnote content.
+
+### Lists
+
+#### Unordered
+
+* Item 1
+* Item 2
+* Item 3
+
+#### Ordered
+
+1. Item 1
+1. Item 2
+1. Item 3
+
+### Links
+
+[Link to Kromatic.fr](https://kromatic.fr)
+
+### Images
+
+![Logo Kromatic](https://kromatic.fr/wp-content/uploads/2019/08/KLogoComplet_Smaller.png)
+
+
+### Blockquotes
+
+> Blockquote
+
+### Tables
+
+| Header 1 | Header 2 |
+| -------- | -------- |
+| Cell 1 | Cell 2 |
+| Cell 3 | Cell 4 |
+
+### Horizontal Rule
+
+---
+
+`;
+
 function App() {
   const [markdown, setMarkdown] = useState('');
 
@@ -103,9 +171,7 @@ function App() {
   );
 
   function retrieveInitialMarkdown() {
-    fetch('/instructions.md')
-      .then(response => response.text())
-      .then(data => setMarkdown(data));
+    setMarkdown(instructions);
   }
 }
 
