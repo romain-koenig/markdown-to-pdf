772881a0ed818ea8ad9f01a6690540bc39ff8d2f
Romain KOENIG <romain@kromatic.fr>
2023-05-16 18:00:40 +0200

    Giving credit where credit is due

--- src/About.js
+++ src/About.js
@@ -9,12 +9,15 @@ function About() {
 			<p><img height="32" width="32" src="https://kromatic.fr/wp-content/uploads/2019/08/KLogo_512_carré.png" alt="Kromatic logo" /> Created by Romain Koenig / <a href="https://kromatic.fr" target="_blank" rel="noopener noreferrer">Kromatic</a></p>
 			<p><img height="32" width="32" src="https://cdn.simpleicons.org/github/181717" alt="GitHub logo" /> Hosted on GitHub Pages</p>
 
-			<p>Welcome to my pet project, the Markdown to PDF Converter.
+			<p>Welcome to one of my pet projects, the Markdown to PDF Converter.<br />
 				This project was born out of my passion for learning and my desire to deepen my understanding of React and GitHub Pages.
 				It represents 24 hours, from idea to MVP, of coding, and debugging – all with the aim to create a useful tool (at least for myself) and improve my development skills.
-				As you explore this app, please handle it with care.
-				Your feedback and suggestions are always welcome as they contribute to my learning process.
+				As you explore this app, please handle it with care.<br />
+				Your feedback and suggestions are always welcome as they contribute to my learning process.<br />
 				Enjoy converting your Markdown to PDF, and thank you for being a part of this!</p>
+			<p>More work from me on my <a href="https://portfolio.krokee.fr" target="_blank" rel="noopener noreferrer">Portfolio</a>.</p>
+
+			<p>This application was developed with assistance from ChatGPT and GitHub Copilot, both language models developed by <a href="https://openai.com/" target="_blank" rel="noopener noreferrer">OpenAI</a>.</p>
 
 		</div>
 
