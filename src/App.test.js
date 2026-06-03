f99ca59736b0941478af863551df33528a329298
Romain KOENIG <romain@kromatic.fr>
2026-06-03 14:55:20 +0200

    Updates

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders Markdown to PDF Converter heading', () => {
-  render(<App />);
-  const headingElement = screen.getByText(/Markdown to PDF Converter/i);
-  expect(headingElement).toBeInTheDocument();
-});
